f096f185a8fe8f45c69719b60e94c8d0bfc4c8e7
gustavomathielo <gustavomathielo@hotmail.com>
2020-12-05 17:06:11 -0300

    Erro login

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,7 @@ import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.componen
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HttpClientModule} from '@angular/common/http';
-  import { from } from 'rxjs';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +30,7 @@ import { HttpClientModule} from '@angular/common/http';
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
